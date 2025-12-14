f2d522cc9c9dd18f14a887fdb20a9612ae860c16
ZanGrant <mgrantpangau@gmail.com>
2025-12-14 16:23:43 +0700

    Penyesuaian live server

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware global
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","http://eduna.infinitelearningstudent.id"],
   credentials: true
 }));
 app.use(express.json());
